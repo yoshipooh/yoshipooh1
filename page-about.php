9e8004bf580b0cdbdb5e3b59102ba139e955f297
yoshipooh <57375652+yoshipooh@users.noreply.github.com>
2019-12-11 15:58:21 +0700

    Create page-about.php

---
+++ page-about.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+    
+  <h1 class="headline">当園について</h1>
+  <div class="main-container flex">
+    <img class="main-image" src="<?php echo get_template_directory_uri(); ?>/img/intro.png">
+    <div class="main-text">
+      <p class="bs">〇〇県〇〇郡、標高800メートル、
+         人口3000人弱の小さな村に私たちの農園はあります。
+         緑豊かなこの土地で、土や品種に徹底的にこだわりながら
+         有機野菜を育てています。</p>
+        
+      <p class="bs">新しいモノ・コト・農業の可能性を考え、
+         農業の力で社会的な課題を解決するコトで
+         新たな価値提供していきます。</p>
+        
+      <p>・・・ちょっと真面目に語ってしまいましたが。
+         野菜にかけた愛情は食べた人に必ず伝わります。
+         真面目すぎるくらいがいいんです。</p>
+    </div>
+  </div><!-- /.main-inner -->
+
+  <img class="reversi-partition" src="<?php echo get_template_directory_uri(); ?>/img/top-img/bridge.png" alt="partition">
+
+  <div class="gaiyo">
+    <div class="gaiyo-container">
+      <h1 class="title">会社概要</h1>
+      <img class="mini-partition" src="<?php echo get_template_directory_uri(); ?>/img/top-img/heading-bg.png" alt="partition">
+      <h4 class="content-text"><span>しま農園は</span><span>野菜のことで頭がいっぱいの、</span><span>とにかく真面目な会社です。</span></h4>
+
+      <table class="kaisha">
+        <tr>
+        <th>社名</th>
+        <td>有限会社しま農園</td>
+        </tr>
+        <tr>
+        <th>創立</th>
+        <td>1993年</td>
+        </tr>
+        <tr>
+        <th>所在地</th>
+        <td>〇〇県〇〇郡〇〇村</td>
+        </tr>
+        <tr>
+        <th>資本金</th>
+        <td>1000万人</td>
+        </tr>
+        <tr>
+        <th>従業員数</th>
+        <td>50人</td>
+        </tr>
+      </table>
+
+    </div><!-- /.gaiyo-container -->
+  </div><!-- /.gaiyo -->
+
+  <?php get_footer(); ?>
