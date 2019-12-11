79b50b15e0ac797d6d693c98a69502367ce09f1f
yoshipooh <57375652+yoshipooh@users.noreply.github.com>
2019-12-11 15:59:13 +0700

    Create page-store.php

---
+++ page-store.php
@@ -0,0 +1,83 @@
+<?php get_header(); ?>
+
+  <h1 class="headline">品目・ストア</h1>
+  <div class="store-container">
+    <h4 class="content-text"><span>しま農園のかわいい子たちを</span><span>紹介します。</span></h4>
+    <div class="item flex">
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind1.jpg" alt="item-image1">
+        <h4 class="item-text">ほうれん草</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind2.jpg" alt="item-image1">
+        <h4 class="item-text">トマト</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind3.jpg" alt="item-image1">
+        <h4 class="item-text">たまねぎ</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind4.jpg" alt="item-image1">
+        <h4 class="item-text">レタス</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind5.jpg" alt="item-image1">
+        <h4 class="item-text">キャベツ</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind6.jpg" alt="item-image1">
+        <h4 class="item-text">なす</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind7.jpg" alt="item-image1">
+        <h4 class="item-text">にんじん</h4>
+      </div><!-- /.item-content -->
+      <div class="item-content">
+        <img class="item-image" src="<?php echo get_template_directory_uri(); ?>/img/kind8.jpg" alt="item-image1">
+        <h4 class="item-text">ブロッコリー</h4>
+      </div><!-- /.item-content -->
+    </div><!-- /.item -->
+  </div><!-- /.store-container -->
+
+  <img class="reversi-partition" src="<?php echo get_template_directory_uri(); ?>/img/top-img/bridge.png" alt="partition">
+
+  <div class="store-inner">
+    <div class="online-store-container">
+      <div class="content-head">
+        <h1 class="title">オンラインストア</h1>
+        <img class="mini-partition" src="<?php echo get_template_directory_uri(); ?>/img/top-img/heading-bg.png" alt="partition">
+        <h4 class="content-text"><span>しま農園では毎週火曜日に</span><span>定期便を配達しています。</span></h4>
+      </div><!-- /.content-head -->
+
+      <div class="set">
+        <div class="set-container flex">
+          <div class="set-content">
+            <img class="set-image" src="<?php echo get_template_directory_uri(); ?>/img/top-img/store1.jpg" alt="s-set1">
+            <p class="set-title"><span class="marker1">おてがる<span class="set-size">S</span>セット</span></p>
+            <p class="set-price">1480円(税込)</p>
+            <h4 class="number">2〜3人用</h4>
+            <h4>旬の野菜５種</h4>
+          </div><!-- /.set-content -->
+          <div class="set-content">
+            <img class="set-image" src="<?php echo get_template_directory_uri(); ?>/img/top-img/store2.jpg" alt="m-set">
+            <p class="set-title"><span class="marker1">わいわい<span class="set-size">M</span>セット</span></p>
+            <p class="set-price">1980円(税込)</p>
+            <h4 class="number">5〜6人用</h4>
+            <h4>旬の野菜7〜8種</h4>
+          </div><!-- /.set-content -->
+          <div class="set-content">
+            <img class="set-image" src="<?php echo get_template_directory_uri(); ?>/img/top-img/store3.jpg" alt="l-set">
+            <p class="set-title"><span class="marker1">まんぷく<span class="set-size">L</span>セット</span></p>
+            <p class="set-price">2980円(税込)</p>
+            <h4 class="number">7〜8人用</h4>
+            <h4>旬の野菜10〜12種</h4>
+          </div><!-- set-content -->
+        </div><!-- /.set-container -->
+      </div><!-- /.set -->
+      <div class="buttons">
+        <button class="button">もっと見る</button>
+      </div><!-- /.buttons -->
+    </div><!-- /.online-store-container -->
+  </div><!-- /.inner -->
+
+  <?php get_footer(); ?>
