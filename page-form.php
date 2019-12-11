f9267c81da3ba7fbf1c9c8adeeaf00b04834b3b7
yoshipooh <57375652+yoshipooh@users.noreply.github.com>
2019-12-11 15:58:42 +0700

    Create page-form.php

---
+++ page-form.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+  <h1 class="headline">お問い合わせ</h1>
+  <div class="form-container">
+    <h4 class="content-text"><span>どんなことでもお気軽に</span><span>お問い合わせください。</span></h4>
+    
+
+<div class="entry-content">
+  <?php echo do_shortcode('[contact-form-7 id="17" title="コンタクトフォーム 1"]'); ?>
+</div>
+
+  </div><!-- /.form-container -->
+
+  <?php get_footer(); ?>
